c0236f22d06adf0da3a9cb6e8e3fbc3880b51736
paivaismael <ismaellpaiva@gmail.com>
2015-11-06 12:42:45 -0800

    Updating the test file with instructions to print some statements in case of failure

--- test_attractor.py
+++ test_attractor.py
@@ -29,27 +29,27 @@ class TestRandomValues:
         self.a=Attractor(s,p,b,start,end,points)
     def test_defintions(self):
         """Test the first definitions of the class"""
-        assert self.a.params[0]==self.s
-        assert self.a.params[1]==self.p
-        assert self.a.params[2]==self.b
-        assert self.a.dt==(self.end-self.start)/self.points
+        assert self.a.params[0]==self.s, "\nError in the definition of self.params. The first argument should coincide with the value of s, the first input of the class.\n"
+        assert self.a.params[1]==self.p, "\nError in the definition of self.params. The second argument should coincide with the value of p, the second input of the class.\n"
+        assert self.a.params[2]==self.b, "\nError in the definition of self.params. The third argument should coincide with the value of b, the third input of the class.\n"
+        assert self.a.dt==(self.end-self.start)/self.points, "\nError in the definition of self.dt.\n"
     def test_ks(self):
         """Test if the methods euler, rk2 and rk4 are generating the correct answer"""
         self.a=Attractor(1.,1.,1.,0.,80.,100)
         c=np.array([2,3,4])
-        assert self.a.euler(c)[0]==1.
-        assert self.a.euler(c)[1]==-9.
-        assert self.a.euler(c)[2]==2.
-        assert self.a.rk2(c)[0]==-3.
-        assert self.a.rk2(c)[1]==-8.52
-        assert self.a.rk2(c)[2]==-6.24
-        assert self.a.rk4(c)[0]==1.97024
-        assert int(self.a.rk4(c)[1])==-4
-        assert int(self.a.rk4(c)[2])==0
+        assert self.a.euler(c)[0]==1., "\nError in the first element of the np.array output of euler method.\n"
+        assert self.a.euler(c)[1]==-9., "\nError in the second element of the np.array output of euler method.\n"
+        assert self.a.euler(c)[2]==2., "\nError in the third element of the np.array output of euler method.\n"
+        assert self.a.rk2(c)[0]==-3., "\nError in the first element of the np.array output of rk2 method.\n"
+        assert self.a.rk2(c)[1]==-8.52, "\nError in the second element of the np.array output of rk2 method.\n"
+        assert self.a.rk2(c)[2]==-6.24, "\nError in the third element of the np.array output of rk2 method.\n"
+        assert self.a.rk4(c)[0]==1.97024, "\nError in the first element of the np.array output of rk4 method.\n"
+        assert int(self.a.rk4(c)[1])==-4, "\nError in the second element of the np.array output of rk4 method.\n"
+        assert int(self.a.rk4(c)[2])==0, "\nError in the third element of the np.array output of rk4 method.\n"
     def test_solve(self):
         """Test if solve is printing in the CSV file"""
         os.remove('save_solution.csv')
         self.a.save()
         data=open('save_solution.csv','r')
         d=data.read()
-        assert len(d)>0
+        assert len(d)>0, "\nError in the save method. It is not creating the save_solution.csv file or saving the information of self.solution in it.\n"
